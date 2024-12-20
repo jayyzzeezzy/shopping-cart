60b7d57646bc6d5953d2151ef526c8a03d7d28ac
James Zhou <jam9es@gmail.com>
2024-12-20 10:48:08 -0800

    Test out fetch from the Fake Store API

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,4 +1,21 @@
+import { useState, useEffect } from "react";
+
 function Shop() {
+    const [fakeStoreAPI, setFakeStoreAPI] = useState('');
+
+
+    useEffect(() => {
+        fetch(
+            "https://fakestoreapi.com/products", 
+            {mode: "cors"},
+        )
+        .then(response => response.json())
+        .then(json => setFakeStoreAPI(json))
+        .catch(error => console.error(error))
+    }, []);
+    console.log(fakeStoreAPI);
+    
+
     return (
         <>
             <h1>SHOP</h1>
