cd2acdfe8cfcac98e591bc4ce940e0d220b5cb4e
James Zhou <jam9es@gmail.com>
2024-12-24 14:01:45 -0800

    Write Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+import "./Navbar.module.css";
+
+function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="shop">Shop</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+};
+
+export default Navbar;
