c2536abe9004167d1cdb17a8a348aa900376248f
James Zhou <jam9es@gmail.com>
2024-12-25 11:48:57 -0800

    Add context prop in the outlet component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
+import { useState } from "react"
 import { Outlet } from "react-router-dom"
 import Navbar from "./components/Navbar"
 
 function App() {
+  const [cartList, setCartList] = useState([]);
 
   return (
     <>
       <Navbar />
       
-      <Outlet />
+      <Outlet context={[cartList, setCartList]}/>
     </>
   )
 }
