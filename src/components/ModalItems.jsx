f4dfb19556e1c8abd1f4290a6559cfa88a9672d5
James Zhou <jam9es@gmail.com>
2024-12-28 20:25:16 -0800

    Write increment, decrement, and delete functions for ModalItems

--- src/components/ModalItems.jsx
+++ src/components/ModalItems.jsx
@@ -1,23 +1,76 @@
 import PropTypes from "prop-types";
 import styles from './ModalItems.module.css';
+import { useEffect, useState } from "react";
 
 function ModalItems({
     cartList,
-    // eslint-disable-next-line no-unused-vars
     setCartList,
 }) {
+    const [subtotal, setSubtoal] = useState();
+
+
+    useEffect(() => {
+        if (cartList.length > 0) {
+            let tempTotal = cartList.reduce((acc, item) => {
+                return acc + item.quantity * item.price;
+            }, 0);
+            setSubtoal(tempTotal);
+        } else {
+            setSubtoal(0);
+        }
+    }, [cartList]);
+
+    function incItem(id) {
+        setCartList((prevList) =>{
+            const updatedList = prevList.map((item) => {
+                if (item.id == id) {
+                    return {...item, quantity: item.quantity + 1}
+                }
+                return item;
+            })
+            return updatedList;
+        })
+    }
+
+    function decItem(id) {
+        setCartList((prevList) => {
+            const updatedList = prevList.map((item) => {
+                if (item.id == id) {
+                    return {...item, quantity: item.quantity - 1}
+                }
+            })
+            return updatedList;
+        })
+    }
+
+
+    function delItem(id) {
+        setCartList((prevList) => {
+            return prevList.filter((item) => item.id != id);
+        })
+    }
+
     return (
         <>
         <h3>Items</h3>
+        <h3>{subtotal}</h3>
         
         <ul>
             {cartList.map((item) => {
+                if (!item) return console.log(null);
+
                 return (
                     <li className={styles.modalItem} key={item.id}>
                         <img src={item.img} alt="" />
+                        <p>${item.price}</p>
                         <div className="modalItemEdit">
-                            <input type="number" value={item.quantity}/>
-                            x
+                            {/* remove decrement if quantity is 0 */}
+                            {(item.quantity == 0) ? null : (<button onClick={() => decItem(item.id)}>-</button>)}
+                            <p>{item.quantity}</p>
+                            <button onClick={() => incItem(item.id)}>+</button>
+                        </div>
+                        <div className="modalItemDelete">
+                            <button onClick={() => delItem(item.id)}>&#128465;</button>
                         </div>
                     </li>
                 )
